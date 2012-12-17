0a260109d6ca7fb16144eddc2e980a4ff25c213b
Peter J Smith <pjstarifa@gmail.com>
2012-12-17 15:48:05 +0000

    Added some new services and directives for development

---
+++ app/scripts/mongolab.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: pjs
+ * Date: 17/12/12
+ * Time: 15:33
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ app/scripts/tabs.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: pjs
+ * Date: 17/12/12
+ * Time: 15:34
+ * To change this template use File | Settings | File Templates.
+ */
